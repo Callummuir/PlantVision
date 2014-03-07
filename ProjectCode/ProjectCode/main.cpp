9a1139e9cb985e69b68d7be7816f64c6667280a8
Callum Muir <steppinyello@gmail.com>
2014-03-07 16:10:57 +0000

    Sobel filter on image

--- ProjectCode/ProjectCode/main.cpp
+++ ProjectCode/ProjectCode/main.cpp
@@ -1,41 +1,130 @@
-#include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include "opencv2/core/core_c.h"
-#include "opencv2/core/core.hpp"
-#include "opencv2/imgproc/imgproc_c.h"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/video/tracking.hpp"
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/flann/flann.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/legacy/compat.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+
 
 using namespace cv;
 using namespace std;
 
+
+
+/**
+ * Edge detector, this will detect all edges in the image, use a x-axis kernel
+ * and a y-axis kernel, then add the two images magnitudes together to get
+ * a full
+ *
+ * Param:img - image to run the filter over
+ * Param:output_x - output image in the x direction
+ * Param:output_y - output image in the y direction
+ * Param:output_mag - output maginitued image
+ * Param:output_dir -
+ **/
+void sobel(Mat& img, Mat& output_x, Mat& output_y, Mat& output_mag, Mat& output_dir)
+{
+    output_x.create(img.size(), img.type());
+    output_y.create(img.size(), img.type());
+    double xkernel[3][3] = {{-1,0,1} ,   {-2,0,2} , {-1,0,1}};
+    double ykernel[3][3] = {{-1,-2,-1} , {0,0,0} ,  {1,2,1}};
+    
+    Mat kernel_x = Mat( 3, 3, CV_64F, xkernel );
+    Mat kernel_y = Mat( 3, 3, CV_64F, ykernel );
+    
+    // we need to create a padded version of the input
+	// or there will be border effects
+	int kernelRadiusX = 1, kernelRadiusY = 1;
+    
+	cv::Mat paddedInput;
+	cv::copyMakeBorder( img, paddedInput,
+                       kernelRadiusX, kernelRadiusX, kernelRadiusY, kernelRadiusY,
+                       cv::BORDER_REPLICATE );
+    
+	// now we can do the convolution
+	for ( int i = 0; i < img.rows; i++ )
+	{
+		for( int j = 0; j < img.cols; j++ )
+		{
+			double sum_x = 0.0, sum_y = 0.0;
+			for( int m = -kernelRadiusX; m <= kernelRadiusX; m++ )
+			{
+				for( int n = -kernelRadiusY; n <= kernelRadiusY; n++ )
+				{
+					// find the correct indices we are using
+					int imagex = i + 1 + m;
+					int imagey = j + 1 + n;
+					int kernelx = m + kernelRadiusX;
+					int kernely = n + kernelRadiusY;
+                    
+					// get the values from the padded image and the kernel
+					int imageval_x = ( int ) paddedInput.at<uchar>( imagex, imagey );
+					double kernalval_x = kernel_x.at<double>( kernelx, kernely );
+                    
+                    int imageval_y = ( int ) paddedInput.at<uchar>( imagex, imagey );
+					double kernalval_y = kernel_y.at<double>( kernelx, kernely );
+                    
+					// do the multiplication
+					sum_x += imageval_x * kernalval_x;
+					sum_y += imageval_y * kernalval_y;
+				}
+			}
+			// set the output value as the sum of the convolution
+			output_x.at<uchar>(i, j) = (uchar) MIN(255,MAX(0, (sum_x+255)/2));
+            output_y.at<uchar>(i, j) = (uchar) MIN(255,MAX(0, (sum_y+255)/2));
+		}
+	}
+    
+}
+
+
 int main( int argc, char** argv )
 {
     
-    Mat image1;
-    Mat greyImage;
-    char* imLoc1 = "/Users/callummuir/Documents/4thYear/Project/Code/ProjectCode/Images/plant1.jpg";
-    image1 = imread(imLoc1, CV_LOAD_IMAGE_COLOR);   // Read the file
+    Mat image1 = imread("/Users/callummuir/Documents/4thYear/Project/Code/ProjectCode/Images/plant1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat image1original;
+    image1.copyTo(image1original);
+    //	Mat coins2 = imread("images/coins2.png", CV_LOAD_IMAGE_GRAYSCALE);
+    //	Mat coins3 = imread("images/coins3.png", CV_LOAD_IMAGE_GRAYSCALE);
     
+    GaussianBlur( image1, image1, Size(3,3), 0, 0, BORDER_DEFAULT );
     
-    cvtColor( image1, greyImage, CV_BGR2GRAY );
+    // My sobel
+    Mat output_x,output_y,combined;
+    sobel(image1,output_x,output_y,output_x,output_x);
+    addWeighted( output_x, 0.5, output_y, 0.5, 0, combined );
     
+	namedWindow("image 1 Sobel X", CV_WINDOW_AUTOSIZE);
+	namedWindow("image 1 Sobel Y", CV_WINDOW_AUTOSIZE);
+	namedWindow("image 1 Original", CV_WINDOW_AUTOSIZE);
+	namedWindow("image 1 CV::Sobel", CV_WINDOW_AUTOSIZE);
+    //	namedWindow("Coins 2", CV_WINDOW_AUTOSIZE);
+    //	namedWindow("Coins 3", CV_WINDOW_AUTOSIZE);
     
+	imshow("image 1 Sobel X", output_x);
+	imshow("image 1 Sobel Y", output_y);
+	imshow("image 1 Original", image1original);
+    imshow("image 1 CV::Sobel", combined);
+    moveWindow("image 1 Sobel X", 0, 400);
+    moveWindow("image 1 Sobel Y", 400, 0);
+    moveWindow("image 1 CV::Sobel", 400, 400);
+    //	imshow("Coins 2", coins2);
+    //	imshow("Coins 3", coins3);
     
-    namedWindow( "Orig", CV_WINDOW_AUTOSIZE );
-    namedWindow( "Gray image", CV_WINDOW_AUTOSIZE );
+	waitKey();
     
-    imshow( "Orignal", image1 );
-    imshow( "Gray image", greyImage );
-    
-    
-    
-    waitKey(0);                                          // Wait for a keystroke in the window
-    return 0;
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
